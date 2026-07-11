0f72b73bbd4c85c1f9e721ca84427761aba08f6e
sanginenivamshi21-hub <sanginenivamshi21@gmail.com>
2026-07-11 22:37:06 +0530

    Time: 7 ms (44.97%), Space: 55.6 MB (46.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        for (int i=0;i<nums.length;i++){
-            int key=nums[i];
-            int count = 1;
-            int j=i+1;
-            while(j<nums.length){
-                if (nums[j]==nums[i])
-                    count++;
-                j++;
-            }
-            if (count>nums.length/2)
-                return nums[i];
-        }
-        return -1;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
