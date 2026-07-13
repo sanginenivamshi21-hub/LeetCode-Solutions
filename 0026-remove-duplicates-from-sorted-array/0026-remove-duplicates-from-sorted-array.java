d2f3b8e92f1ae58079c79c90a57725d06adbf5d7
sanginenivamshi21-hub <sanginenivamshi21@gmail.com>
2026-07-13 21:54:26 +0530

    Time: 0 ms (100%), Space: 46.9 MB (24.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,17 +1,12 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
+     public int removeDuplicates(int[] nums) {
         int start =0;
         for (int i=1;i<nums.length;i++){
             if(nums[start]!=nums[i]){
                 start++;
-                swap(nums,start,i);
+                nums[start]=nums[i];
             }
         }
         return start+1;
-    }
-    void swap(int[] nums,int start,int i){
-        int temp=nums[start];
-        nums[start]=nums[i];
-        nums[i]=temp;
-    }
+}
 }
