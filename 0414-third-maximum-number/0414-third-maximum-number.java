fc07f5a010e6b7fa0522fbe84fdf2dad72c72673
sanginenivamshi21-hub <sanginenivamshi21@gmail.com>
2026-07-18 23:04:25 +0530

    Time: 7 ms (34.13%), Space: 45.6 MB (23.43%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -3,7 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int count=1,i;
         int max=nums.length-1;
-        if (nums.length<=2) return nums[nums.length-1];
         for(i=nums.length-2;i>=0;i--){
             if(nums[max]!=nums[i]){
                 max=i;
