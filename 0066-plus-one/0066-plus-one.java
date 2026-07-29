5a759032a0a642a6bf0242d731d6f7616e3c7d44
Sangineni Vamshi <sanginenivamshi21@gmail.com>
2026-07-29 23:50:19 +0530

    Time: 0 ms (100%), Space: 43.4 MB (42.97%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -5,8 +5,7 @@ class Solution {
                 digits[i]++;
                 return digits;
             }
-            else
-                digits[i]=0;
+            digits[i]=0;
         }
         int[] result=new int[digits.length+1];
         result[0]=1;
