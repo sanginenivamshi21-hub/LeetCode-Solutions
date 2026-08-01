fce9d5abe464596bb311d8263c7ccaac14a55a1b
Sangineni Vamshi <sanginenivamshi21@gmail.com>
2026-08-01 23:00:22 +0530

    Time: 1 ms (76.72%), Space: 42.5 MB (51.67%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int digit;
+        HashSet<Integer> seen=new HashSet<>();
+        while(true){
+            int res=0;
+            while(n>0){
+            digit=n%10;
+            res+=(int)Math.pow(digit,2);
+            n=n/10;
+        }
+        n=res;
+        if(seen.contains(n)) break;
+        else seen.add(n);
+        if (n==1) return true;
+        }
+        return false;
+    }
+}
