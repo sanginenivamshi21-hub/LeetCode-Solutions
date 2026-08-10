fbfbaf5842fdaad2b62845d02e0950332a3c3b7a
Sangineni Vamshi <sanginenivamshi21@gmail.com>
2026-08-10 21:40:07 +0530

    Time: 0 ms (100%), Space: 46.3 MB (55.84%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.java
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.java
@@ -2,8 +2,7 @@ class Solution {
     public int averageValue(int[] nums) {
         int res=0,count=0;
         for (int i:nums){
-            if(i%3!=0) continue;
-            if(i%2==0){
+            if(i%6==0){
                 res +=i;
                 count++;
             }
