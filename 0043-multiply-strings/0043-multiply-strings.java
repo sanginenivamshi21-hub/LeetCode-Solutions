552e609fdee3e9458cfbae9c27012dc7089402ee
Sangineni Vamshi <sanginenivamshi21@gmail.com>
2026-08-15 20:48:56 +0530

    Time: 3 ms (79.45%), Space: 43.9 MB (21.01%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,11 +1,11 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int m=num1.length();
-        int n=num2.length();
+        int x=num1.length();
+        int y=num2.length();
         if (num1.equals("0") || num2.equals("0")) return "0";
-        int[] res=new int[m+n];
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
+        int[] res=new int[x+y];
+        for(int i=x-1;i>=0;i--){
+            for(int j=y-1;j>=0;j--){
                 int a=num1.charAt(i)-'0';
                 int b=num2.charAt(j)-'0';
                 int product=a*b;
