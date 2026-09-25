bbe7a415ce8da8a926e9e8c687ebcac27f1f0e5e
Sangineni Vamshi <sanginenivamshi21@gmail.com>
2026-09-25 21:01:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,10 +1,12 @@
 class Solution {
+    class Solution {
     public char findTheDifference(String s, String t) {
-        int res1=0,res2=0;
+        int res=0,res2=0;
         for(int i=0;i<s.length();i++)
-           res1+=(int)s.charAt(i);
+           res+=(int)s.charAt(i);
         for(int i=0;i<t.length();i++)
             res2 +=(int)t.charAt(i);
-        return (char)(res2-res1);
+        return (char)(res2-res);
     }
+}
 }
